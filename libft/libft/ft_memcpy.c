346b738726e17b490e8f66b10cce8beb1eca1304
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2021-09-24 13:54:13 +0200

    DONE

---
+++ libft/libft/ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanfeli <jsanfeli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 13:14:54 by jsanfeli          #+#    #+#             */
+/*   Updated: 2021/09/22 13:41:53 by jsanfeli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t len)
+{
+	char		*d;
+	const char	*s;
+
+	d = dest;
+	s = src;
+	while (len--)
+		*d++ = *s++;
+	return (dest);
+}
